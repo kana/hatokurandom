5674b17389760f1cc06cbba2c8d2b86444a04e9d
Kana Natsuno <dev@whileimautomaton.net>
2012-05-10 00:29:01 +0900

    Sort CARDS for further extension

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,51 +1,55 @@
 (function ($) {
-  var CARDS = [  //{{{
-    {name: '斥候', type: '行動・攻撃（兵力）', cost: 2, link: 2, set: '基本セット'},
+  var CARDS = [  // Sorted by set, cost, then name.  //{{{
     {name: '城壁', type: '行動・防衛', cost: 2, link: 1, set: '基本セット'},
-    {name: '早馬', type: '行動', cost: 2, link: 2, set: '基本セット'},
     {name: '寄付', type: '行動', cost: 2, link: 1, set: '基本セット'},
+    {name: '斥候', type: '行動・攻撃（兵力）', cost: 2, link: 2, set: '基本セット'},
+    {name: '早馬', type: '行動', cost: 2, link: 2, set: '基本セット'},
     {name: '願いの泉', type: '行動', cost: 2, link: 1, set: '基本セット'},
-    {name: 'お金好きの妖精', type: '行動（魔法）', cost: 2, link: 0, set: '極東辺境領'},
 
+    {name: '交易船', type: '行動（商人）', cost: 3, link: 0, set: '基本セット'},
     {name: '召集令状', type: '行動', cost: 3, link: 1, set: '基本セット'},
-    {name: '隠れ家', type: '行動・防衛', cost: 3, link: 1, set: '基本セット'},
-    {name: '買収工作', type: '行動・攻撃（計略）', cost: 3, link: 1, set: '基本セット'},
-    {name: '魔法の護符', type: '行動・防衛・呪い', cost: 3, link: 1, set: '基本セット'},
     {name: '埋もれた財宝', type: '行動', cost: 3, link: 0, set: '基本セット'},
     {name: '御用商人', type: '行動（商人）', cost: 3, link: 0, set: '基本セット'},
     {name: '焼き畑農業', type: '行動', cost: 3, link: 1, set: '基本セット'},
-    {name: '交易船', type: '行動（商人）', cost: 3, link: 0, set: '基本セット'},
     {name: '破城槌', type: '行動', cost: 3, link: 1, set: '基本セット'},
-    {name: '貿易商人', type: '行動（魔法）', cost: 3, link: 0, set: '極東辺境領'},
-    {name: '伝書鳩', type: '行動（計略）', cost: 3, link: 1, set: '極東辺境領'},
-    {name: '課税', type: '行動', cost: 3, link: 0, set: '極東辺境領'},
-    {name: '弓兵隊', type: '行動（兵力）', cost: 3, link: 1, set: '極東辺境領'},
+    {name: '買収工作', type: '行動・攻撃（計略）', cost: 3, link: 1, set: '基本セット'},
+    {name: '隠れ家', type: '行動・防衛', cost: 3, link: 1, set: '基本セット'},
+    {name: '魔法の護符', type: '行動・防衛・呪い', cost: 3, link: 1, set: '基本セット'},
 
-    {name: '追い立てられた魔獣', type: '行動・攻撃（計略）', cost: 4, link: 1, set: '基本セット'},
-    {name: '星詠みの魔女', type: '行動（魔法）', cost: 4, link: 2, set: '基本セット'},
-    {name: '補給部隊', type: '行動（兵力）', cost: 4, link: 2, set: '基本セット'},
-    {name: '図書館', type: '行動', cost: 4, link: 1, set: '基本セット'},
-    {name: '都市開発', type: '行動（商人）', cost: 4, link: 1, set: '基本セット'},
-    {name: '魅了術の魔女', type: '行動・攻撃（魔法）', cost: 4, link: 1, set: '基本セット'},
-    {name: '歩兵大隊', type: '行動・攻撃（兵力）', cost: 4, link: 0, set: '基本セット'},
-    {name: '金貸し', type: '行動（商人）', cost: 4, link: 1, set: '基本セット'},
     {name: 'シノビ', type: '行動（計略）', cost: 4, link: 2, set: '基本セット'},
-    {name: '港町', type: '領地', cost: 4, link: 2, set: '極東辺境領'},
-    {name: '鉱山都市', type: '領地', cost: 4, link: 1, set: '極東辺境領'},
-    {name: '見習い魔女', type: '行動・攻撃（魔法）', cost: 4, link: 1, set: '極東辺境領'},
-    {name: 'クノイチ', type: '行動・防衛（計略）', cost: 4, link: 1, set: '極東辺境領'},
-    {name: 'サムライ', type: '行動・攻撃（兵力）', cost: 4, link: 0, set: '極東辺境領'},
+    {name: '図書館', type: '行動', cost: 4, link: 1, set: '基本セット'},
+    {name: '星詠みの魔女', type: '行動（魔法）', cost: 4, link: 2, set: '基本セット'},
+    {name: '歩兵大隊', type: '行動・攻撃（兵力）', cost: 4, link: 0, set: '基本セット'},
+    {name: '補給部隊', type: '行動（兵力）', cost: 4, link: 2, set: '基本セット'},
+    {name: '追い立てられた魔獣', type: '行動・攻撃（計略）', cost: 4, link: 1, set: '基本セット'},
+    {name: '都市開発', type: '行動（商人）', cost: 4, link: 1, set: '基本セット'},
+    {name: '金貸し', type: '行動（商人）', cost: 4, link: 1, set: '基本セット'},
+    {name: '魅了術の魔女', type: '行動・攻撃（魔法）', cost: 4, link: 1, set: '基本セット'},
 
+    {name: '冒険者', type: '行動', cost: 5, link: 0, set: '基本セット'},
+    {name: '呪詛の魔女', type: '行動・攻撃（魔法）', cost: 5, link: 0, set: '基本セット'},
+    {name: '皇室領', type: '継承権・領地', cost: 5, link: 1, set: '基本セット'},
     {name: '近衛騎士団', type: '行動・攻撃（兵力）', cost: 5, link: 0, set: '基本セット'},
     {name: '銀行', type: '行動（商人）', cost: 5, link: 0, set: '基本セット'},
-    {name: '皇室領', type: '継承権・領地', cost: 5, link: 1, set: '基本セット'},
-    {name: '呪詛の魔女', type: '行動・攻撃（魔法）', cost: 5, link: 0, set: '基本セット'},
-    {name: '冒険者', type: '行動', cost: 5, link: 0, set: '基本セット'},
     {name: '錬金術師', type: '行動', cost: 5, link: 1, set: '基本セット'},
-    {name: '結盟', type: '行動', cost: 5, link: 2, set: '極東辺境領'},
-    {name: '割り符', type: '行動（商人）', cost: 5, link: 0, set: '極東辺境領'},
 
-    {name: '噂好きの公爵夫人', type: '継承権', cost: 6, link: 0, set: '基本セット'}
+    {name: '噂好きの公爵夫人', type: '継承権', cost: 6, link: 0, set: '基本セット'},
+
+    {name: 'お金好きの妖精', type: '行動（魔法）', cost: 2, link: 0, set: '極東辺境領'},
+
+    {name: '伝書鳩', type: '行動（計略）', cost: 3, link: 1, set: '極東辺境領'},
+    {name: '弓兵隊', type: '行動（兵力）', cost: 3, link: 1, set: '極東辺境領'},
+    {name: '課税', type: '行動', cost: 3, link: 0, set: '極東辺境領'},
+    {name: '貿易商人', type: '行動（魔法）', cost: 3, link: 0, set: '極東辺境領'},
+
+    {name: 'クノイチ', type: '行動・防衛（計略）', cost: 4, link: 1, set: '極東辺境領'},
+    {name: 'サムライ', type: '行動・攻撃（兵力）', cost: 4, link: 0, set: '極東辺境領'},
+    {name: '港町', type: '領地', cost: 4, link: 2, set: '極東辺境領'},
+    {name: '見習い魔女', type: '行動・攻撃（魔法）', cost: 4, link: 1, set: '極東辺境領'},
+    {name: '鉱山都市', type: '領地', cost: 4, link: 1, set: '極東辺境領'},
+
+    {name: '割り符', type: '行動（商人）', cost: 5, link: 0, set: '極東辺境領'},
+    {name: '結盟', type: '行動', cost: 5, link: 2, set: '極東辺境領'}
   ];  //}}}
   var CARD_TABLE =
     (function () {
