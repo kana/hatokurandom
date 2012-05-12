08f0ae94661328973f9ba484fab91e9b6c901745
Kana Natsuno <dev@whileimautomaton.net>
2012-05-12 14:42:24 +0900

    Prepare for writing custom specs

---
+++ spec/app.js
@@ -0,0 +1,10 @@
+describe('Core', function () {
+});
+
+
+
+
+// __END__
+// vim: expandtab shiftwidth=2 softtabstop=2
+// vim: foldmethod=expr
+// vim: foldexpr=getline(v\:lnum)=~#'\\v<x?(describe|it)>.*<function>\\s*\\([^()]*\\)\\s*\\{'?'a1'\:(getline(v\:lnum)=~#'^\\s*});'?'s1'\:'=')
