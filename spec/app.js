c5e6c44344033b00b842ab68d9eb793ac82d78c9
Kana Natsuno <dev@whileimautomaton.net>
2012-05-16 18:58:52 +0900

    Add spec/app.js skeleton

---
+++ spec/app.js
@@ -0,0 +1,10 @@
+(function (H, $) {
+})(hatokurandom, jQuery);
+
+
+
+
+// __END__
+// vim: expandtab shiftwidth=2 softtabstop=2
+// vim: foldmethod=expr
+// vim: foldexpr=getline(v\:lnum)=~#'\\v<x?(describe|it|beforeEach|afterEach)>.*<function>\\s*\\([^()]*\\)\\s*\\{'?'a1'\:(getline(v\:lnum)=~#'^\\s*});'?'s1'\:'=')
