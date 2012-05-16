8e2188c2022daa27a9fb6cea527f6f845ded420c
Kana Natsuno <dev@whileimautomaton.net>
2012-05-16 18:29:49 +0900

    Create the application skeleton

---
+++ app.rb
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+require 'bundler/setup'
+require 'cgi'
+require 'haml'
+require 'sinatra'
+
+
+
+
+class App < Sinatra::Application
+  NAME = 'ハトクランダム'
+  GITHUB_REPOS_URI = 'https://github.com/kana/hatokurandom'
+
+  get '/' do
+    haml :index
+  end
+end
+
+
+
+
+__END__
---
+++ public/javascripts/app.js
@@ -0,0 +1,25 @@
+var hatokurandom = {};
+
+(function (H, $) {
+  // Naming convensions  //{{{1
+  //
+  // cid: Card ID
+  // eid: Expansion ID
+  // sid: Supply ID
+  // rsid: Random Supply ID
+  // psid: Predefined Supply ID
+  // tid: Template ID
+  // pid: Page ID
+
+  // Constants  //{{{1
+
+  // Utilities  //{{{1
+
+  // Bootstrap  //{{{1
+  $(document).ready(function () {
+  });  //}}}1
+})(hatokurandom, jQuery);
+
+// __END__  {{{1
+// vim: expandtab shiftwidth=2 softtabstop=2
+// vim: foldmethod=marker
