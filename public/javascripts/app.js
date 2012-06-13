81b532c9f90abae3028d3765a4bfd074c835e311
Kana Natsuno <dev@whileimautomaton.net>
2012-06-13 23:52:52 +0900

    Add H.xcards_from_rsid

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -699,6 +699,48 @@ var hatokurandom = {};
     });
   };
 
+  H.xcards_from_rsid = function (rsid) {  //{{{2
+    // An rsid is a string which is encoded from xcards with BASE64XML,
+    // so that each character in rsid is corresponding to a 6-bit value.
+    // To simplify both encoding and decoding,
+    // we use only an array of 6-bit values to convert rsid from/to xcards.
+    //
+    // The first character in an rsid represents a version of rsid/xcards
+    // conversion.  There is only one version at the moment,
+    // and the version is 0x01 ('A').
+    //
+    // The rest of characters in an rsid represent xcards.
+    //
+    // A xcard is represented with 12-bit value.
+    // The most significant bit represents a dropped status,
+    // where 0 means available and 1 means dropped.
+    // The rest bits represent a cid.
+    // Other information of a xcard is retrieved by H.card_from_cid.
+
+    var bs = H.decode_base64xml(rsid);
+    var xcards = [];
+
+    var version = bs.shift();
+    if (version != 0x01)
+      throw new H.Error(JSON.stringify(version) + ' is not a valid version.');
+
+    while (2 <= bs.length) {
+      var b1 = bs.shift();
+      var b2 = bs.shift();
+      var dropped = !!(b1 & (1 << 5));
+      var cid = ((b1 & ((1 << 5) - 1)) << 5) | b2;
+      xcards.push($.extend({dropped: dropped}, H.card_from_cid(cid)));
+    }
+    if (bs.length != 0) {
+      throw new H.Error([
+        JSON.stringify(rsid), 'is not valid RSID;',
+        'it contains trailing data:', JSON.stringify(bs)
+      ].join(' '));
+    }
+
+    return xcards;
+  };
+
   // Core  //{{{1
   H.prepare_supplies_page = function (e, data, pid) {  //{{{2
     var meta = H.meta_from_pid(pid);
--- spec/app.js
+++ spec/app.js
@@ -201,6 +201,27 @@
       expect(xcards[0]).not.toBe(H.card_from_cid(xcards[0].cid));
     });
   });
+  describe('xcards_from_rsid', function () {
+    it('should return xcards from rsid', function () {
+      expect(H.xcards_from_rsid('B')).toEqual([]);
+      expect(H.xcards_from_rsid('BAB')).toEqual([
+        $.extend({dropped: false}, H.card_from_cid(0x01))
+      ]);
+      expect(H.xcards_from_rsid('BADgc')).toEqual([
+        $.extend({dropped: false}, H.card_from_cid(0x03)),
+        $.extend({dropped: true}, H.card_from_cid(0x1c)),
+      ]);
+    });
+    it('should reject if rsid contains an invalid character', function () {
+      expect(function () {H.xcards_from_rsid('B??');}).toThrow();
+    });
+    it('should reject if rsid contains trailing data', function () {
+      expect(function () {H.xcards_from_rsid('BA');}).toThrow();
+    });
+    it('should reject if rsid contains an invalid cid', function () {
+      expect(function () {H.xcards_from_rsid('BAA');}).toThrow();
+    });
+  });
 })(hatokurandom, jQuery);
 
 
