d7f3aa7e879879e5b6e96397937fafef8b9b192f
Kana Natsuno <dev@whileimautomaton.net>
2012-07-07 01:51:08 +0900

    H.rsid_from_xcards: Add more tests on cid >= 0x40

--- spec/app.js
+++ spec/app.js
@@ -381,6 +381,28 @@
         ])
       );
     });
+    it('should return rsid from xcards with cid >= 0x40', function () {
+      // This is a problematic case.  Because cards with cid >= 0x40 consit of
+      // 7 or more bits and require 2 character in BASE64.
+      expect(
+        H.rsid_from_xcards([
+          {dropped: false, cid: 0x3f},
+          {dropped: true, cid: 0x3f},
+          {dropped: false, cid: 0x40},
+          {dropped: true, cid: 0x40},
+          {dropped: false, cid: 0x41},
+          {dropped: true, cid: 0x41},
+          {dropped: false, cid: 0x82},
+          {dropped: true, cid: 0x82},
+          {dropped: false, cid: 0x104},
+          {dropped: true, cid: 0x104},
+          {dropped: false, cid: 0x208},
+          {dropped: true, cid: 0x208},
+          {dropped: false, cid: 0x410},
+          {dropped: true, cid: 0x410}
+        ])
+      ).toEqual('BA-g-BAhABBhBCCiCEEkEIIoIQQwQ');
+    });
   });
   describe('sid_from_pid', function () {
     it('should return sid from a pid', function () {
