e88445f488fdc21a9140c6c060b4504af4636eda
Kana Natsuno <dev@whileimautomaton.net>
2012-07-25 18:49:17 +0900

    Refine 'foldexpr' for closing lines
    ... to avoid confusion with "});" lines for $.each etc.

--- spec/app.js
+++ spec/app.js
@@ -802,4 +802,4 @@
 // __END__
 // vim: expandtab shiftwidth=2 softtabstop=2
 // vim: foldmethod=expr
-// vim: foldexpr=getline(v\:lnum)=~#'\\v<x?(describe|it|beforeEach|afterEach)>.*<function>\\s*\\([^()]*\\)\\s*\\{'?'a1'\:(getline(v\:lnum)=~#'^\\s*});'?'s1'\:'=')
+// vim: foldexpr=getline(v\:lnum)=~#'\\v<x?(describe|it|beforeEach|afterEach)>.*<function>\\s*\\([^()]*\\)\\s*\\{'?'a1'\:(getline(v\:lnum)=~#'^\\s*});$'?'s1'\:'=')
