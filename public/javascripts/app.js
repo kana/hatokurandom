7ab61dff59dfba93bf60ac5b57912f3aa7bd396f
Kana Natsuno <dev@whileimautomaton.net>
2014-05-17 22:39:42 +0900

    Adjust availability of #header itself

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1860,6 +1860,7 @@ var hatokurandom = {};
 
   // Core  //{{{1
   H.adjust_header = function ($page) {  //{{{2
+    $('#header').toggleClass('disabled', $page.attr('id') == 'configure');
     $('#header .reshuffle.button').toggleClass(
       'disabled',
       !H.is_dsid($page.jqmData('sid'))
