35e98dee7c0aab4f6a0cceee68559b7abb1c5f20
Kana Natsuno <dev@whileimautomaton.net>
2019-06-24 21:26:03 +0900

    Add /about

---
+++ pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page">
+    <ul class="link-list">
+      <li class="link-item">
+        <nuxt-link to="/">
+          Home
+        </nuxt-link>
+      </li>
+      <li class="link-item">
+        <nuxt-link to="/expansion/basic">
+          Basic
+        </nuxt-link>
+      </li>
+      <li class="link-item">
+        <nuxt-link to="/about">
+          About this app
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.page {
+  background: #fee;
+}
+</style>
