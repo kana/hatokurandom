4f8df741990c372b2cddbb94fae3d89e6cf7c728
Kana Natsuno <dev@whileimautomaton.net>
2019-06-24 21:24:41 +0900

    Rewrite the default page with a skeleton

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,31 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        hatokurandom
-      </h1>
-      <h2 class="subtitle">
-        A utility to choose a supply for HEART of CROWN.
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="page">
+    <ul class="link-list">
+      <li class="link-item">
+        <nuxt-link to="/">
+          Home
+        </nuxt-link>
+      </li>
+      <li class="link-item">
+        <nuxt-link to="/expansion/basic">
+          Basic
+        </nuxt-link>
+      </li>
+      <li class="link-item">
+        <nuxt-link to="/about">
+          About this app
+        </nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped>
+.page {
+  background: #eef;
 }
 </style>
