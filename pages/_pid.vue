68418672fcb80569ac4d805d5b793ee78df3b80f
Kana Natsuno <dev@whileimautomaton.net>
2019-07-27 12:44:16 +0900

    Implement list pages

---
+++ pages/_pid.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page">
+    <ul class="link-list">
+      <li v-for="menu in menus" :key="menu.pid" class="link-item">
+        <nuxt-link :to="menu.pid">
+          {{ menu.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { childPidListFromPid, titleFromPid } from '../lib/constants'
+
+export default {
+  validate ({ params }) {
+    return childPidListFromPid([params.pid]) !== undefined
+  },
+  computed: {
+    menus () {
+      return childPidListFromPid(this.$route.params.pid).map(pid => ({
+        pid,
+        title: titleFromPid(pid)
+      }))
+    }
+  }
+}
+</script>
+
+<style scoped>
+.page {
+  background: #eef;
+}
+</style>
