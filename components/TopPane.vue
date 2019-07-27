44f5c873f4e3d93f23855ee9646ed0076812d18f
Kana Natsuno <dev@whileimautomaton.net>
2019-07-27 22:37:22 +0900

    Implement share button basic

--- components/TopPane.vue
+++ components/TopPane.vue
@@ -6,20 +6,57 @@
     <div class="title">
       {{ title }}
     </div>
+    <a v-if="shareable" :href="shareUrl" class="share-button" target="_blank">
+      [^]
+    </a>
   </div>
 </template>
 
 <script>
-import { parentPidFromPid, pidFromPath, titleFromPid } from '~/lib/constants'
+import { isCardListPid, parentPidFromPid, pidFromPath, titleFromPid, xcardsFromPid } from '~/lib/constants'
 
 export default {
   name: 'TopPane',
   computed: {
+    locationOrigin () {
+      return 'https://hatokurandom.whileimautomaton.net'
+    },
     parentPid () {
-      return parentPidFromPid(pidFromPath(this.$route.path))
+      return parentPidFromPid(this.pid)
+    },
+    pid () {
+      return pidFromPath(this.$route.path)
+    },
+    shareable () {
+      return isCardListPid(this.pid) && this.sharePid
+    },
+    sharePid () {
+      // return this.$store.state.supply.pid
+      return 'supply:BAFAJAKgZAbAeAqhCBDBhB-CM' // TODO
+    },
+    shareUrl () {
+      const permalink = `${this.locationOrigin}/${this.sharePid}`
+      const isReferencePage = /^reference:/.test(this.sharePid)
+      const usedCardNames = xcardsFromPid(this.sharePid)
+        .filter(xcard => !xcard.dropped)
+        .map(xcard => xcard.name)
+      const baseMessage = isReferencePage
+        ? `ハトクラの${this.title}`
+        : `ハトクラなう。今回のサプライ: ${usedCardNames.join(', ')}`
+      const optionSharingTool = 'web_intent' // TODO
+      const ss = optionSharingTool === 'web_intent' ? [
+        'https://twitter.com/intent/tweet',
+        '?url=', encodeURIComponent(permalink),
+        '&text=', encodeURIComponent(`${baseMessage} #hatokura`),
+        '&related=', encodeURIComponent('HeartofCrown')
+      ] : [
+        'twitter://post?message=',
+        encodeURIComponent(`${baseMessage} #hatokura ${permalink}`)
+      ]
+      return ss.join('')
     },
     title () {
-      return titleFromPid(pidFromPath(this.$route.path))
+      return titleFromPid(this.pid)
     }
   },
   methods: {
@@ -53,6 +90,15 @@ export default {
   height: 1em;
   margin-left: 1em;
   margin-right: -2em;
+  width: 1em;
+}
+
+.share-button {
+  cursor: pointer;
+  flex: none;
+  height: 1em;
+  margin-left: -2em;
+  margin-right: 1em;
   width: 1em;
 }
 
