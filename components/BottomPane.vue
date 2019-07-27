9cd3a66b5004009602df305256ccc1daeb71a2b7
Kana Natsuno <dev@whileimautomaton.net>
2019-07-28 00:58:25 +0900

    Add basic bottom pane

---
+++ components/BottomPane.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bottom-pane">
+    <nuxt-link to="/" class="tab">
+      [Home]
+    </nuxt-link>
+    <nuxt-link to="/configure" class="tab">
+      [Preferences]
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BottomPane'
+}
+</script>
+
+<style scoped>
+
+.bottom-pane {
+  align-items: center;
+  background: #eee;
+  border-top: 1px solid #ddd;
+  display: flex;
+  height: 3em;
+  justify-content: center;
+}
+
+.tab {
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+}
+
+.tab + .tab {
+  border-left: 1px solid #ddd;
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
   <div class="app">
     <top-pane />
     <nuxt />
+    <bottom-pane />
   </div>
 </template>
 
 <script>
+import BottomPane from '~/components/BottomPane'
 import TopPane from '~/components/TopPane'
 
 export default {
   components: {
+    BottomPane,
     TopPane
   }
 }
