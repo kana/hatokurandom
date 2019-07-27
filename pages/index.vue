bebdc18c1da7ce81d7e108190f6420c8a461edd1
Kana Natsuno <dev@whileimautomaton.net>
2019-07-27 11:42:56 +0900

    Separate menus

--- pages/index.vue
+++ pages/index.vue
@@ -11,14 +11,12 @@
 </template>
 
 <script>
+import { menus } from '../lib/constants'
+
 export default {
   computed: {
     menus () {
-      return [
-        { pid: 'basic', name: '推奨サプライ(基本セット)' },
-        { pid: 'fareast', name: '推奨サプライ(極東辺境領)' },
-        { pid: 'northern', name: '推奨サプライ(北限の魔女)' }
-      ]
+      return menus
     }
   }
 }
