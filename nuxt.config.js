3e63666b87582fd580af0d5585882bf37611a2af
Kana Natsuno <dev@whileimautomaton.net>
2019-07-27 19:17:42 +0900

    Fix "Unexpected token export"

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,6 +50,9 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {},
+    transpile: [
+      'lodash-es'
+    ]
   }
 }
