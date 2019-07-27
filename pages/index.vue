1b27fa9e3459b2f2a48e471eb8aff87f26cb89de
Kana Natsuno <dev@whileimautomaton.net>
2019-07-27 12:07:33 +0900

    Define pid-to-child-pid-list table

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="page">
     <ul class="link-list">
-      <li v-for="menu in menus" :key="menu.pid" class="link-item">
-        <nuxt-link :to="menu.pid">
-          {{ menu.name }}
+      <li v-for="pid in childPidList" :key="pid" class="link-item">
+        <nuxt-link :to="pid">
+          {{ pid }}
         </nuxt-link>
       </li>
     </ul>
@@ -11,12 +11,12 @@
 </template>
 
 <script>
-import { menus } from '../lib/constants'
+import { childPidListFromPid } from '../lib/constants'
 
 export default {
   computed: {
-    menus () {
-      return menus
+    childPidList () {
+      return childPidListFromPid('home')
     }
   }
 }
