3a140769f1857a964d6594af1fd796a8fadcf8d7
Kana Natsuno <dev@whileimautomaton.net>
2019-07-27 16:03:48 +0900

    Support old URLs

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,20 @@
 
 <script>
 import PageList from '~/components/PageList'
+import { isCardListPid, isPageListPid } from '~/lib/constants'
 
 export default {
   components: {
     PageList
+  },
+  mounted () {
+    // Redirect old URL (/#supply:random10) to new URL (/supply:random10).
+    if (this.$route.hash !== '') {
+      const pid = this.$route.hash.slice(1)
+      if (isCardListPid(pid) || isPageListPid(pid)) {
+        this.$router.replace(`/${pid}`)
+      }
+    }
   }
 }
 </script>
