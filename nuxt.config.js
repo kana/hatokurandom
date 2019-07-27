2161a4cbf7c585c5c7a397a74ff88e93e8993896
Kana Natsuno <dev@whileimautomaton.net>
2019-07-27 19:23:02 +0900

    Set friendly meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'ハトクランダム',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'ハートオブクラウンを楽しく遊ぶ為のランダマイザです。'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
