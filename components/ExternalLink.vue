986a7e42a48bc78be6bc771c312802bbeb9b9df2
Kana Natsuno <dev@whileimautomaton.net>
2019-07-28 22:00:33 +0900

    Add ExternalLink

---
+++ components/ExternalLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :href="href" target="_blank">
+    {{ label }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ExternalLink',
+  props: {
+    href: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -2,13 +2,13 @@
   <div class="page">
     <p>
       このアプリは
-      <a href="http://flipflops.jp/" target="_blank">FLIPFLOPs</a>
+      <external-link href="http://flipflops.jp/" label="FLIPFLOPs" />
       様製作のデッキ成長型カードゲーム
-      <a href="http://hatokura.flipflops.jp/" target="_blank">Heart of Crown</a>
+      <external-link href="http://hatokura.flipflops.jp/" label="Heart of Crown" />
       用のサプライを選ぶためのツールです。
     </p>
     <p>
-      <a href="http://hatokura.flipflops.jp/rule" target="_blank">ルールブック</a>
+      <external-link href="http://hatokura.flipflops.jp/rule" label="ルールブック" />
       には初めてプレイする際に最適な
       <nuxt-link to="supplies:basic">
         推奨サプライ
@@ -23,28 +23,32 @@
     </p>
     <p>
       この手のツールは
-      <a href="https://www.google.com/search?q=%E3%83%8F%E3%83%BC%E3%83%88%E3%82%AA%E3%83%96%E3%82%AF%E3%83%A9%E3%82%A6%E3%83%B3%20%E3%83%A9%E3%83%B3%E3%83%80%E3%83%A0%20%E9%81%B8%E6%8A%9E" target="_blank">既存の実装</a>
+      <external-link href="https://www.google.com/search?q=%E3%83%8F%E3%83%BC%E3%83%88%E3%82%AA%E3%83%96%E3%82%AF%E3%83%A9%E3%82%A6%E3%83%B3%20%E3%83%A9%E3%83%B3%E3%83%80%E3%83%A0%20%E9%81%B8%E6%8A%9E" label="既存の実装" />
       がいくつも存在しますが、
-      <a href="https://twitter.com/kana1" target="_blank">作者</a>
+      <external-link href="https://twitter.com/kana1" label="作者" />
       が
-      <a href="https://nuxtjs.org/" target="_blank">普段使用していない技術</a>
+      <external-link href="https://nuxtjs.org/" label="普段使用していない技術" />
       を使っての習作と私的な需要を満たすことを兼ねて作成しています。
     </p>
     <ul>
       <li>
-        <a href="https://github.com/kana/hatokurandom" target="_blank">ソースコード</a>
+        <external-link href="https://github.com/kana/hatokurandom" label="ソースコード" />
       </li>
       <li>
-        <a href="https://twitter.com/intent/tweet?text=@kana1+&amp;hashtags=hatokurandom" target="_blank">ご意見・ご感想</a>
+        <external-link href="https://twitter.com/intent/tweet?text=@kana1+&amp;hashtags=hatokurandom" label="ご意見・ご感想" />
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import ExternalLink from '~/components/ExternalLink'
 import { titleFromPid } from '~/lib/constants'
 
 export default {
+  components: {
+    ExternalLink
+  },
   head: {
     title: titleFromPid('about')
   }
