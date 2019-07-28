c3247771d7d299bd20caff11f1a4ba4b69e42e85
Kana Natsuno <dev@whileimautomaton.net>
2019-07-28 16:31:12 +0900

    Show card meta

--- components/CardListItem.vue
+++ components/CardListItem.vue
@@ -2,12 +2,17 @@
   <li class="card">
     <label class="label">
       <input v-if="editable" v-show="false" v-model="xcard.dropped" type="checkbox">
-      {{ xcard.name }}
+      <span class="cost">{{ xcard.cost }}</span>
+      <span class="types">{{ xcard.types }}</span>
+      <span class="name">{{ xcard.name }}</span>
+      <span class="expansion" :data-symbol="expansionSymbol">{{ expansionSymbol }}</span>
     </label>
   </li>
 </template>
 
 <script>
+import { expansionFromEid } from '~/lib/constants'
+
 export default {
   name: 'CardListItem',
   props: {
@@ -19,6 +24,11 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    expansionSymbol () {
+      return expansionFromEid(this.xcard.eid).symbol
+    }
   }
 }
 </script>
@@ -26,8 +36,57 @@ export default {
 <style scoped>
 
 .label {
+  align-items: center;
   cursor: pointer;
+  display: flex;
+  justify-content: flex-start;
   width: 100%;
+}
+
+.name {
+  width: 100%;
+}
+
+.expansion {
+  border-radius: 0.25em;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+  flex: none;
+  font-size: 80%;
+  line-height: 100%;
+  padding: 0.25em 0.25em;
+}
+
+.expansion[data-symbol='基本'] {
+  color: #666;
+  background: #ddd;
+}
+.expansion[data-symbol='極東'] {
+  color: #884;
+  background: #eeb;
+}
+.expansion[data-symbol='北限'] {
+  color: #669;
+  background: #cce;
+}
+.expansion[data-symbol='ＦＧ'] {
+  color: #696;
+  background: #ded;
+}
+.expansion[data-symbol='六都'] {
+  color: #966;
+  background: #ece;
+}
+.expansion[data-symbol='星天'] {
+  color: #642;
+  background: #fc9;
+}
+.expansion[data-symbol='幕間'] {
+  color: #636;
+  background: #fde;
+}
+.expansion[data-symbol='レ！'] {
+  color: #663;
+  background: #ee9;
 }
 
 </style>
