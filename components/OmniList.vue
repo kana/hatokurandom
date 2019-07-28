a8ee0d17a710bc9c1e93cc566c03f4f4cead7269
Kana Natsuno <dev@whileimautomaton.net>
2019-07-28 18:22:19 +0900

    Separate OmniList

---
+++ components/OmniList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="list">
+    <slot />
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'OmniList'
+}
+</script>
+
+<style scoped>
+
+.list {
+  background: var(--item-background-color);
+  border-color: var(--item-separator-color);
+  border-style: solid none;
+  border-width: 1px;
+  list-style: none;
+  margin: 2em 0;
+  padding: 0;
+}
+
+</style>
--- components/PageList.vue
+++ components/PageList.vue
@@ -1,19 +1,23 @@
 <template>
-  <ul class="list">
+  <omni-list>
     <li v-for="childPid in childPids" :key="childPid" class="list-item">
       <nuxt-link :to="pathFromPid(childPid)" class="link">
         <span class="label">{{ titleFromPid(childPid) }}</span>
         <span class="icon">＞</span>
       </nuxt-link>
     </li>
-  </ul>
+  </omni-list>
 </template>
 
 <script>
+import OmniList from '~/components/OmniList'
 import { childPidsFromPid, pathFromPid, titleFromPid } from '~/lib/constants'
 
 export default {
   name: 'PageList',
+  components: {
+    OmniList
+  },
   props: {
     pid: {
       type: String,
@@ -33,16 +37,6 @@ export default {
 </script>
 
 <style scoped>
-
-.list {
-  background: var(--item-background-color);
-  border-color: var(--item-separator-color);
-  border-style: solid none;
-  border-width: 1px;
-  list-style: none;
-  margin: 2em 0;
-  padding: 0;
-}
 
 .list-item {
   margin: 0;
