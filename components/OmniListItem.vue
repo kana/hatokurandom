8a2fcd54c22eee44fd8d4e3ca2991a571f3732bb
Kana Natsuno <dev@whileimautomaton.net>
2019-07-28 20:30:59 +0900

    Forgot to add OmniListItem

---
+++ components/OmniListItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <li class="list-item">
+    <component :is="props.is" v-bind="props" class="list-item-content">
+      <slot />
+    </component>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'OmniListItem',
+  props: {
+    props: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.list-item {
+  margin: 0;
+  padding: 0;
+}
+
+.list-item + .list-item {
+  border-top: 1px solid var(--item-separator-color);
+}
+
+.list-item-content {
+  color: var(--item-label-color);
+  padding: 0.5em 1em;
+  text-decoration: none;
+  display: block;
+  transition: background 0.2s;
+}
+
+.list-item-content:hover {
+  background: var(--header-background-color);
+}
+
+</style>
