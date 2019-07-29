711283f8f618d1085b48f1c53c88563e83844c42
Kana Natsuno <dev@whileimautomaton.net>
2019-07-30 00:25:23 +0900

    Add Preferences page skeleton

---
+++ pages/preferences.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page">
+    - 使用するカードセット
+    - 禁止カード
+    - 偏り具合の調整
+    - 共有方法
+  </div>
+</template>
+
+<script>
+import { titleFromPid } from '~/lib/constants'
+
+export default {
+  head: {
+    title: titleFromPid('preferences')
+  }
+}
+</script>
+
+<style scoped>
+
+.page {
+  margin: 2em 5%;
+}
+
+</style>
