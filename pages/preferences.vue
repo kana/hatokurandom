7c30769449585046d7552408b6699d61fc7c6f79
Kana Natsuno <dev@whileimautomaton.net>
2019-07-30 01:03:22 +0900

    Write basic template for each preference item

--- pages/preferences.vue
+++ pages/preferences.vue
@@ -1,18 +1,126 @@
 <template>
   <div class="page">
-    - 使用するカードセット
-    - 禁止カード
-    - 偏り具合の調整
-    - 共有方法
+    <block-title>
+      使用するカードセット
+    </block-title>
+    <ul>
+      <li v-for="expansion in configurableExpansions" :key="expansion.eid">
+        <div>{{ expansion.name }}</div>
+        <div :id="expansion.optionKey">
+          <label><input :name="expansion.optionKey" type="radio"> 必須</label>
+          <label><input :name="expansion.optionKey" type="radio"> 使用</label>
+          <label><input :name="expansion.optionKey" type="radio"> 禁止</label>
+        </div>
+      </li>
+    </ul>
+
+    <block-title>
+      禁止カード
+    </block-title>
+    <div>
+      <div id="must_exclude_cards">
+        サムライ, 割り符
+      </div>
+      <div>
+        ＞
+      </div>
+    </div>
+
+    <block-title>
+      偏り具合の調整
+    </block-title>
+    <ul>
+      <li>
+        <label>
+          <input name="exclude_banned_cards" type="checkbox" checked>
+          <div class="title">
+            公式推奨バランスにする
+          </div>
+          <div class="description">
+            以下のカードは出現しません:
+            <span class="card-name">埋もれた財宝</span>
+            <span class="card-name">買収工作</span>
+            <span class="card-name">魅了術の魔女</span>
+          </div>
+        </label>
+      </li>
+      <li>
+        <label>
+          <input name="exclude_banned_cards_for_fairy_garden" type="checkbox" checked>
+          <div class="title">
+            フェアリーガーデンと併用非推奨のカードを除外する
+          </div>
+          <div class="description">
+            フェアリーガーデンのカードと以下のカードは同時に出現しません:
+            全ての防衛カード、および
+            <span class="card-name">破城槌</span>
+            <span class="card-name">埋もれた財宝</span>
+            <span class="card-name">星詠みの魔女</span>
+            <span class="card-name">シノビ</span>
+            <span class="card-name">魅了術の魔女</span>
+            <span class="card-name">歩兵大隊</span>
+            <span class="card-name">近衛騎士団</span>
+            <span class="card-name">弓兵隊</span>
+            <span class="card-name">サムライ</span>
+          </div>
+        </label>
+      </li>
+      <li>
+        <label>
+          <input name="exclude_banned_cards_for_trajectory_of_the_star" type="checkbox" checked>
+          <div class="title">
+            星天前路と併用非推奨のカードを除外する
+          </div>
+          <div class="description">
+            星天前路のカードと以下のカードは同時に出現しません:
+            「フェアリーガーデンと併用非推奨のカード」一式、および
+            <span class="card-name">割り符</span>
+          </div>
+        </label>
+      </li>
+      <li>
+        <label>
+          <input name="include_all_costs" type="checkbox">
+          <div class="title">
+            各コスト帯のカードを含める
+          </div>
+          <div class="description">
+            各コスト帯のカードを1枚以上含めるようにします。
+            ただし6コスト以上のカードは5コスト帯として扱います。
+          </div>
+        </label>
+      </li>
+      <li>
+        <label>
+          <input name="include_link_2" type="checkbox">
+          <div class="title">
+            リンク2を適宜含める
+          </div>
+          <div class="description">
+            サプライにリンク0のカードが含まれる場合、
+            リンク2のカードを1枚は含めるようにします。
+          </div>
+        </label>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { titleFromPid } from '~/lib/constants'
+import BlockTitle from '~/components/BlockTitle'
+import { EXPANSIONS, titleFromPid } from '~/lib/constants'
 
 export default {
+  components: {
+    BlockTitle
+  },
   head: {
     title: titleFromPid('preferences')
+  },
+  computed: {
+    configurableExpansions () {
+      return EXPANSIONS.filter(expansion => expansion.optionKey !== undefined)
+    }
   }
 }
 </script>
