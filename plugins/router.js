2500ffe7eb8ac1d81ac150da5fd6626ee994143f
Kana Natsuno <dev@whileimautomaton.net>
2019-07-30 05:57:45 +0900

    Track fullPath for homeTabLastPath

--- plugins/router.js
+++ plugins/router.js
@@ -1,7 +1,7 @@
 export default ({ app }) => {
   app.router.afterEach((to, from) => {
     if (!to.path.startsWith('/preferences')) {
-      app.store.dispatch('history/homeTabLastPath', to.path)
+      app.store.dispatch('history/homeTabLastPath', to.fullPath)
     }
   })
 }
