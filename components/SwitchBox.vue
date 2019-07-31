e78eeb97c95c3c99c592cc281fb171dc1a32bc90
Kana Natsuno <dev@whileimautomaton.net>
2019-07-31 23:39:21 +0900

    Twitch SwitchBox knob size

--- components/SwitchBox.vue
+++ components/SwitchBox.vue
@@ -31,13 +31,14 @@ export default {
 <style scoped>
 
 .switch-box {
+  --unit: calc(1.728em);
   background: var(--switch-off-background-color);
-  border-radius: 1.44em;
+  border-radius: var(--unit);
   border: 1px solid var(--switch-off-border-color);
   box-sizing: border-box;
-  height: 1.44em;
+  height: var(--unit);
   transition: background 0.2s, border-color 0.2s;
-  width: calc((1.44em - 4px) * 2);
+  width: calc((var(--unit) - 4px) * 2);
 }
 
 .switch-box.on {
@@ -47,17 +48,17 @@ export default {
 
 .knob {
   background: var(--switch-knob-color);
-  border-radius: 1.44em;
+  border-radius: var(--unit);
   box-sizing: border-box;
   box-shadow: rgba(0, 0, 0, 0.1) 1px 1px 3px 1px;
-  height: calc(1.44em - 2px);
+  height: calc(var(--unit) - 2px);
   margin-left: 0;
   transition: margin-left 0.2s;
-  width: calc(1.44em - 2px);
+  width: calc(var(--unit) - 2px);
 }
 
 .switch-box.on .knob {
-  margin-left: calc((1.44em - 4px) - 4px);
+  margin-left: calc((var(--unit) - 4px) - 4px);
 }
 
 </style>
