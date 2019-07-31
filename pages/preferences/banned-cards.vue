33ab0467d13e260a87292868e61456c7cbc5985e
Kana Natsuno <dev@whileimautomaton.net>
2019-08-01 00:29:18 +0900

    Add empty preferences/banned-cards page

---
+++ pages/preferences/banned-cards.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page">
+    ...
+  </div>
+</template>
+
+<script>
+import { titleFromPid } from '~/lib/constants'
+
+export default {
+  head: {
+    title: titleFromPid('preferences/banned-cards')
+  }
+}
+</script>
+
+<style scoped>
+
+.page {
+  margin: 2em 0;
+}
+
+</style>
