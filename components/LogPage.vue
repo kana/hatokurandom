0f9c83d5f4b30ca7bbf6aa5e481ecbce2734a7a1
Kana Natsuno <dev@whileimautomaton.net>
2019-08-03 18:03:58 +0900

    Pass "at" to PageListItem

--- components/LogPage.vue
+++ components/LogPage.vue
@@ -1,10 +1,11 @@
 <template>
   <page-list>
     <page-list-item
-      v-for="childPid in childPids"
-      :key="childPid"
-      :path="pathFromPid(childPid)"
-      :title="titleFromPid(childPid)"
+      v-for="item in items"
+      :key="item.pid"
+      :path="item.path"
+      :title="item.title"
+      :at="item.at"
     />
   </page-list>
 </template>
@@ -20,13 +21,35 @@ export default {
     PageListItem
   },
   computed: {
-    childPids () {
-      return this.$store.state.log.items.map(item => pidFromSid(item.sid))
+    items () {
+      return this.$store.state.log.items.map((item) => {
+        const pid = pidFromSid(item.sid)
+        return {
+          pid,
+          path: pathFromPid(pid),
+          title: titleFromPid(pid),
+          at: this.formatDateTime(new Date(item.at))
+        }
+      })
     }
   },
   methods: {
-    pathFromPid,
-    titleFromPid
+    formatDateTime (dateTime) {
+      const yyyy = this.pad(dateTime.getFullYear(), 4)
+      const mm = this.pad(dateTime.getMonth() + 1, 2)
+      const dd = this.pad(dateTime.getDate(), 2)
+      const HH = this.pad(dateTime.getHours(), 2)
+      const MM = this.pad(dateTime.getMinutes(), 2)
+      const SS = this.pad(dateTime.getSeconds(), 2)
+      return `${yyyy}-${mm}-${dd} ${HH}:${MM}:${SS}`
+    },
+    pad (n, width) {
+      let s = n.toString()
+      while (s.length < width) {
+        s = `0${s}`
+      }
+      return s
+    }
   }
 }
 </script>
