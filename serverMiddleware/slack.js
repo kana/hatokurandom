fe3db60dc33aa51f3c9156a33da2a67e95e58b42
Kana Natsuno <dev@whileimautomaton.net>
2019-08-08 01:12:14 +0900

    Test how unfurling works

--- serverMiddleware/slack.js
+++ serverMiddleware/slack.js
@@ -1,3 +1,8 @@
 export default async function (req, res, next) {
-  res.end('')
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({
+    response_type: 'in_channel',
+    text: 'https://hatokurandom.whileimautomaton.net/supply:six-inquisition',
+    unfurl_links: true
+  }))
 }
