deb16a830b755c5559f2475f17f952d7a2ffb79a
Kana Natsuno <dev@whileimautomaton.net>
2019-08-08 22:57:40 +0900

    Use attachments

--- serverMiddleware/slack.js
+++ serverMiddleware/slack.js
@@ -2,7 +2,15 @@ export default async function (req, res, next) {
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify({
     response_type: 'in_channel',
-    text: 'https://hatokurandom.whileimautomaton.net/supply:six-inquisition',
-    unfurl_links: true
+    attachments: [
+      {
+        fallback: '',
+        color: '#edc0ed',
+        title: '異端審問',
+        title_link: 'https://hatokurandom.whileimautomaton.net/supply:six-inquisition',
+        text: '免罪符、十字軍……',
+        image_url: 'https://hatokurandom.whileimautomaton.net/ogp/supply:six-inquisition'
+      }
+    ]
   }))
 }
