024c619669631eecc17cbf335dfdd54fc03bf927
Kana Natsuno <dev@whileimautomaton.net>
2019-08-16 01:10:54 +0900

    Emit leaving-from-randomizer-page

--- plugins/router.js
+++ plugins/router.js
@@ -1,4 +1,17 @@
+import EventBus from '~/lib/eventbus'
+import { parseSpecialPid, pidFromPath } from '~/lib/utils'
+
 export default ({ app }) => {
+  app.router.beforeEach((to, from, next) => {
+    const f = parseSpecialPid(pidFromPath(from.path))
+    const t = parseSpecialPid(pidFromPath(to.path))
+    if (f.random && !t.random) {
+      EventBus.$emit('leaving-from-randomizer-page')
+    }
+
+    next()
+  })
+
   app.router.afterEach((to, from) => {
     if (to.path.startsWith('/preferences')) {
       app.store.dispatch('history/preferencesTabLastPath', to.fullPath)
