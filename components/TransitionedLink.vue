f790e7aabcb2dc1c243494cb3b36cb614a691aa9
Kana Natsuno <dev@whileimautomaton.net>
2020-09-13 02:05:31 +0900

    Add TransitionedLink

---
+++ components/TransitionedLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <nuxt-link :to="normalizedTo">
+    <slot />
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: [String, Object],
+      required: true
+    }
+  },
+  computed: {
+    normalizedTo () {
+      return this.$props.to
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
