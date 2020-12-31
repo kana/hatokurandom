81eb547c7c4837b4b84860b3ba3424086c0fba5b
Kana Natsuno <dev@whileimautomaton.net>
2021-01-01 01:40:21 +0900

    Define <trigger>

---
+++ components/CardText/Trigger.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="trigger">
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+.trigger::before {
+  content: "（";
+}
+
+.trigger::after {
+  content: "）";
+}
+
+.trigger {
+  color: #666;
+}
+
+</style>
