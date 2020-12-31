5c2829a1a3bfa1ff53b953055452a0b8689787bf
Kana Natsuno <dev@whileimautomaton.net>
2020-12-31 23:49:25 +0900

    Define <mode>

---
+++ components/CardText/Mode.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mode">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+.mode::before {
+  content: "「";
+}
+
+.mode::after {
+  content: "」";
+}
+
+</style>
