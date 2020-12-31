48c00fd610ba0ab6e66e741f3b0d87976411cdb4
Kana Natsuno <dev@whileimautomaton.net>
2021-01-01 00:35:31 +0900

    Define <succession-point>

---
+++ components/CardText/SuccessionPoint.vue
@@ -0,0 +1,18 @@
+<template>
+  <span class="succession-point">
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+.succession-point::before {
+  content: "継承点 ";
+}
+
+</style>
