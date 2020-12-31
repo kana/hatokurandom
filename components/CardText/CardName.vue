6b36acb4164c53ea76c1bab838f9b5b364765898
Kana Natsuno <dev@whileimautomaton.net>
2021-01-01 00:01:53 +0900

    Define <card-name>

---
+++ components/CardText/CardName.vue
@@ -0,0 +1,36 @@
+<template>
+  <transitioned-link :path="path" class="card-name">
+    <slot />
+  </transitioned-link>
+</template>
+
+<script>
+import TransitionedLink from '~/components/TransitionedLink'
+import { cidFromCardName } from '~/lib/utils'
+
+export default {
+  components: {
+    TransitionedLink
+  },
+  computed: {
+    cardName () {
+      return this.$slots.default[0].text
+    },
+    path () {
+      return `/cards/${cidFromCardName(this.cardName)}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.card-name::before {
+  content: "「";
+}
+
+.card-name::after {
+  content: "」";
+}
+
+</style>
