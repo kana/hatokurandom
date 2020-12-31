18bc5a3db8ddddd7deef341e8930aae2e0f3e763
Kana Natsuno <dev@whileimautomaton.net>
2021-01-01 01:48:48 +0900

    Define <note>

---
+++ components/CardText/Note.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="note">
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+.note::before {
+  content: "（";
+}
+
+.note::after {
+  content: "）";
+}
+
+.note {
+  font-size: 80%;
+}
+
+</style>
