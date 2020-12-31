5996f50122f8a9bcc2ec56a6879a62b60616e553
Kana Natsuno <dev@whileimautomaton.net>
2021-01-01 01:46:13 +0900

    Define implicit <unit> for readability

---
+++ components/CardText/Unit.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="unit">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+.unit + .unit {
+  margin-top: 1ex;
+}
+
+</style>
