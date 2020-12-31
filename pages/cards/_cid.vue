83640084dc4c0aad0c2caa970488cd94e03d19b8
Kana Natsuno <dev@whileimautomaton.net>
2020-12-31 09:04:04 +0900

    Update template for card detail view

--- pages/cards/_cid.vue
+++ pages/cards/_cid.vue
@@ -1,13 +1,47 @@
 <template>
   <page-container>
-    [[ {{ card.name }} ]]
-    [[ {{ card.types }} ]]
+    <table>
+      <tbody>
+        <tr>
+          <th>名前</th>
+          <td>{{ card.name }}</td>
+        </tr>
+        <tr>
+          <th>コスト</th>
+          <td>{{ card.cost }}</td>
+        </tr>
+        <tr>
+          <th>タイプ</th>
+          <td>{{ card.types.join('、') }}</td>
+        </tr>
+        <tr v-if="card.subtype !== undefined">
+          <th>サブタイプ</th>
+          <td>{{ card.subtype }}</td>
+        </tr>
+        <tr v-if="card.link !== undefined">
+          <th>リンク</th>
+          <td>{{ card.link }}</td>
+        </tr>
+        <tr>
+          <th>レアリティ</th>
+          <td>{{ rarity }}</td>
+        </tr>
+        <tr>
+          <th>収録セット</th>
+          <td>{{ expansionName }}</td>
+        </tr>
+        <tr>
+          <th>テキスト</th>
+          <td>XXX</td>
+        </tr>
+      </tbody>
+    </table>
   </page-container>
 </template>
 
 <script>
 import PageContainer from '~/components/PageContainer'
-import { cardFromCid, pidFromPath, ogpMetaFromPid, titleTagValueFromPid, transition } from '~/lib/utils'
+import { cardFromCid, expansionFromEid, pidFromPath, ogpMetaFromPid, titleTagValueFromPid, transition } from '~/lib/utils'
 
 export default {
   components: {
@@ -30,6 +64,16 @@ export default {
     },
     cid () {
       return parseInt(this.$route.params.cid, 10)
+    },
+    expansionName () {
+      return expansionFromEid(this.card.eid).name
+    },
+    rarity () {
+      return {
+        B: 'ベーシック',
+        C: 'コモン',
+        R: 'レア'
+      }[this.card.rarity] || '?'
     }
   }
 }
